a7d30e6c2c8c06f9290c1b40396b0c8885820f64
Tiffany Simionescu <tiffanyfeldkamp@gmail.com>
2019-09-10 23:06:09 -0400

    Step 1 - Get request using Axios.

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,6 +3,20 @@
            https://api.github.com/users/<your name>
 */
 
+axios
+  .get("https://api.github.com/users/tiffany-simionescu")
+  .then(function(response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function(error) {
+    // handle error
+    console.log(error);
+  })
+  .finally(function() {
+    // always executed
+  });
+
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
